1f297a49ba4e22912d10b3aed3fb318ef829cbfd
Thiago Tonelli Fabris <thifabris@gmail.com>
2024-03-17 18:39:47 -0300

    starting project

---
+++ src/main/java/com/example/sjef/SjefApplication.java
@@ -0,0 +1,13 @@
+package com.example.sjef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sjef/SjefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sjef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
